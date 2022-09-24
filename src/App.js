b56fe0cca6b55ad527b4fb5e2aee4b9153247429
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-09-24 14:22:01 -0500

    Fetch feed data and parse to Json object

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
 import FeedContainer from './Components/FeedContainer';
 import Sidebar from './Components/Sidebar';
+import XMLParser from 'react-xml-parser';
+import { useEffect } from 'react';
+
+
+
 
 function App() {
+
+
+useEffect(() => {
+  fetch("https://feeds.simplecast.com/54nAGcIl")
+      .then(res => res.text())
+      .then(data => {
+          var xml = new XMLParser().parseFromString(data); 
+          console.log(xml.children)
+      })
+      .catch(err => console.log(err));
+}, [])
+
   return (
     <div className="App">
       <Sidebar />
@@ -10,5 +27,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Feed.css';
 
 
+
 export default function Feed() {
   return (
     <div className='feed'>Feed</div>
