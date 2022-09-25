f515674e1554317b0c153f2de79e9f285e2f1869
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-09-25 13:43:59 -0500

    Fetch rss data

--- src/App.js
+++ src/App.js
@@ -1,29 +1,51 @@
 import './App.css';
 import FeedContainer from './Components/FeedContainer';
 import Sidebar from './Components/Sidebar';
-import XMLParser from 'react-xml-parser';
-import { useEffect } from 'react';
-
-
+import { useEffect, useState } from 'react';
 
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [rssUrl, setRssUrl] = useState("");
 
+  const getRss = async (url) => {
+    const res = await fetch(`https://api.allorigins.win/get?url=${rssUrl || url}`);
+    const { contents } = await res.json();
+    const feed = new window.DOMParser().parseFromString(contents, "text/xml");
+    const items = feed.querySelectorAll("item");
+    const feedItems = [...items].map(el => ({
+      // image: el.querySelectorAll("media:content").innerHTML,
+      link: el.querySelector("link").innerHTML,
+      title: el.querySelector("title").innerHTML,
+      description: el.querySelector("description").innerHTML
+    }));
+    setItems(feedItems);
+  };
 
-useEffect(() => {
-  fetch("https://feeds.simplecast.com/54nAGcIl")
-      .then(res => res.text())
-      .then(data => {
-          var xml = new XMLParser().parseFromString(data); 
-          console.log(xml.children)
-      })
-      .catch(err => console.log(err));
-}, [])
+  useEffect(() => {
+    getRss('http://www.npr.org/rss/rss.php?id=1019')
+  })
 
   return (
     <div className="App">
+      
+      {/* {items.map((item) => {
+        return (
+          <div>
+            <h1>{item.title}</h1>
+            <p>{item.description}</p>
+            <a href={item.link}>Full Article</a>
+          </div>
+        );
+      })} */}
       <Sidebar />
-      <FeedContainer />
+      <FeedContainer 
+      items={items}
+      setItems={setItems}
+      rssUrl={rssUrl}
+      setRssUrl={setRssUrl}
+      getRss={getRss}
+      />
     </div>
   );
 }
