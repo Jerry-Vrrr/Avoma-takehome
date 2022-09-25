4ccc517b2fd37b872b9a1407f65a6b739e1b6d38
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-09-25 13:44:16 -0500

    display and style rss feed data

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -3,8 +3,14 @@ import './Feed.css';
 
 
 
-export default function Feed() {
+export default function Feed({title, description, link}) {
   return (
-    <div className='feed'>Feed</div>
+    <div className='feed'>
+      <div>
+        <h1>{title}</h1>
+        <p>{description}</p>
+        <a href={link}>Full Article</a>
+      </div>
+    </div>
   )
 }
