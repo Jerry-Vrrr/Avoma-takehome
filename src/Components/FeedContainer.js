8e99668abc1a032c620797f2e2838b091455269f
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-09-25 13:44:50 -0500

    Map feed and render feed items

--- src/Components/FeedContainer.js
+++ src/Components/FeedContainer.js
@@ -3,17 +3,28 @@ import './FeedContainer.css';
 import Feed from './Feed';
 
 
-export default function FeedContainer() {
+export default function FeedContainer({items, setRssUrl, rssUrl, getRss}) {
+  console.log(items)
+  const feeds = items.map((item) => {
+    return (
+      <Feed 
+            title={item.title}
+            description={item.description}
+            link={item.link}
+      />
+    );
+  })
   return (
     <div className='feed-container'>FeedContainer
-      <Feed />
-      <Feed />
-      <Feed />
-      <Feed />
-      <Feed />
-      <Feed />
-      <Feed />
-      <Feed />
+    <form onSubmit={getRss}>
+        <div>
+          <label> rss url</label>
+          <br />
+          <input onChange={(e) => setRssUrl(e.target.value)} value={rssUrl} />
+        </div>
+        <input type="submit" />
+      </form>
+      {feeds}
     </div>
   )
 }
