c7e6707bef2c17d77b6334a477ec14cba442fe9e
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-09-26 15:43:41 -0500

    Create state to hold session favorites

--- src/App.js
+++ src/App.js
@@ -7,44 +7,41 @@ import { useEffect, useState } from 'react';
 function App() {
   const [items, setItems] = useState([]);
   const [rssUrl, setRssUrl] = useState("");
-
+  const [currentFeed, setCurrentFeed] = useState('Home')
+  const [favorites, setFavorites] = useState([])
+  console.log('fav', favorites)
   const getRss = async (url) => {
-    const res = await fetch(`https://api.allorigins.win/get?url=${rssUrl || url}`);
+    const res = await fetch(`https://api.allorigins.win/get?url=${url}`);
     const { contents } = await res.json();
     const feed = new window.DOMParser().parseFromString(contents, "text/xml");
     const items = feed.querySelectorAll("item");
     const feedItems = [...items].map(el => ({
-      // image: el.querySelectorAll("media:content").innerHTML,
       link: el.querySelector("link").innerHTML,
       title: el.querySelector("title").innerHTML,
-      description: el.querySelector("description").innerHTML
+      description: el.querySelector("description").innerHTML,
     }));
     setItems(feedItems);
   };
 
   useEffect(() => {
-    getRss('http://www.npr.org/rss/rss.php?id=1019')
-  })
+    getRss('http://www.newyorker.com/feed/humor')
+  }, [])
 
   return (
     <div className="App">
-      
-      {/* {items.map((item) => {
-        return (
-          <div>
-            <h1>{item.title}</h1>
-            <p>{item.description}</p>
-            <a href={item.link}>Full Article</a>
-          </div>
-        );
-      })} */}
-      <Sidebar />
+      <Sidebar 
+        getRss={getRss}
+        setCurrentFeed={setCurrentFeed}
+      />
       <FeedContainer 
       items={items}
       setItems={setItems}
       rssUrl={rssUrl}
       setRssUrl={setRssUrl}
       getRss={getRss}
+      currentFeed={currentFeed}
+      favorites={favorites}
+      setFavorites={setFavorites}
       />
     </div>
   );
